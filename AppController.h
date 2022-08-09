a702f820d5843006379dbc25ab606c204af5b055
Noah Wood <noah@pliny.attlocal.net>
2022-08-08 20:11:41 -0400

    Initial commit.

---
+++ AppController.h
@@ -0,0 +1,39 @@
+/* 
+   Project: Columnator
+
+   Author: Noah Wood
+
+   Created: 2022-08-08 20:04:52 -0400 by noah
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+}
+
++ (void)  initialize;
+
+- (id) init;
+- (void) dealloc;
+
+- (void) awakeFromNib;
+
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+- (void) showPrefPanel: (id)sender;
+
+@end
+
+#endif
---
+++ Columnator_main.m
@@ -0,0 +1,30 @@
+/* 
+   Project: Columnator
+
+   Author: Noah Wood
+
+   Created: 2022-08-08 20:04:52 -0400 by noah
+*/
+
+#import <AppKit/AppKit.h>
+
+int 
+main(int argc, const char *argv[])
+{
+// Uncomment if your application is Renaissance application
+/*  CREATE_AUTORELEASE_POOL (pool);
+  [NSApplication sharedApplication];
+  [NSApp setDelegate: [AppController new]];
+
+  #ifdef GNUSTEP
+    [NSBundle loadGSMarkupNamed: @"MainMenu-GNUstep"  owner: [NSApp delegate]];
+  #else
+    [NSBundle loadGSMarkupNamed: @"MainMenu-OSX"  owner: [NSApp delegate]];
+  #endif
+   
+  RELEASE (pool);
+*/
+
+  return NSApplicationMain (argc, argv);
+}
+
